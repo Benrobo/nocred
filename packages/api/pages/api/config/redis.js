1f452e13409413d5b6c1ed319f2deef5817fc9c3
benrobo <alumonabenaiah71@gmail.com>
2023-09-13 22:15:39 +0100

    updated redis config

--- packages/api/pages/api/config/redis.js
+++ packages/api/pages/api/config/redis.js
@@ -1,14 +1,37 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-  url: `redis://${process.env.REDIS_SERVER}`,
-  password: process.env.REDIS_PWD,
+  host: process.env.UPSTASH_REDIS,
+  // legacyMode: true,
 });
 
-redisClient.on("error", (err) => console.log("Redis Client Error", err));
+redisClient
+  .connect()
+  .then(() => {
+    console.log("REDIS CONNECTED");
+  })
+  .catch((err) => {
+    console.log(`REDIS CONNECTION ERROR: ${err.message}`);
+  });
+
+// const redisClient = new Redis(process.env.UPSTASH_REDIS);
+
+// redisClient.on("connect", () => {
+//   console.log("Connected to Redis");
+// });
+
+// redisClient.on("error", (err) => console.log("Redis Client Error", err));
 
 export default redisClient;
 
 export const connectRedis = async () => {
-  await redisClient.connect();
+  if (redisClient && redisClient.status === "ready") {
+    // If the client is already connected and in a ready state, do nothing.
+    return;
+  } else if (redisClient && redisClient.status !== "end") {
+    // If the client exists but is not in a ready state, close it.
+    await redisClient.quit();
+  } else {
+    await redisClient.connect();
+  }
 };
