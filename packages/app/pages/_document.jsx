14bf65c959167741589d724bc84a9583833c9287
benrobo <alumonabenaiah71@gmail.com>
2023-09-14 16:48:04 +0100

    added some seo tags

--- packages/app/pages/_document.jsx
+++ packages/app/pages/_document.jsx
@@ -1,13 +1,50 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Nocred.</title>
+        <meta
+          name="description"
+          content="Secure E-Portal Access Without Credentials"
+        />
+        <meta
+          property="og:title"
+          content="Securely Grant other students access to your noun elearn portal without sharing credentials."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://nocred.vercel.app" />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/Benrobo/nocred/main/md-assets/7.png"
+        />
+        <meta
+          property="og:description"
+          content="Securely Grant other students access to your noun elearn portal without sharing credentials."
+        />
+        <meta property="og:site_name" content="nocred" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@nocred" />
+        <meta
+          name="twitter:title"
+          content="Secure E-Portal Access Without Credentials"
+        />
+        <meta
+          name="twitter:description"
+          content="Securely Grant other students access to your noun elearn portal without sharing credentials."
+        />
+        <meta
+          name="twitter:image"
+          content="https://raw.githubusercontent.com/Benrobo/nocred/main/md-assets/7.png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
