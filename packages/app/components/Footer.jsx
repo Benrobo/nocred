6d9b736a568e997c14e9eaf42a0331e2778aaf04
benrobo <alumonabenaiah71@gmail.com>
2023-09-15 15:24:20 +0100

    added favico and footer

---
+++ packages/app/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { DiGithubBadge } from "react-icons/di";
+
+function Footer() {
+  return (
+    <div className="w-full bg-dark-200">
+      <p className="w-full flex items-center justify-between gap-3 py-7">
+        <div className="w-auto flex items-center justify-between">
+          <span className="text-white-300 text-[13px] flex items-center justify-center ">
+            Created by{" "}
+            <a
+              target="_blank"
+              href="https://github.com/benrobo"
+              className="font-ppB underline ml-2"
+            >
+              Benrobo
+            </a>
+          </span>
+        </div>
+        <div className="w-auto flex items-center justify-between">
+          <a
+            target="_blank"
+            href="https://github.com/benrobo/nocred"
+            className="font-ppB underline ml-2"
+          >
+            <DiGithubBadge className="text-white-100" size={20} />
+          </a>
+        </div>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
--- packages/app/components/Topbar.jsx
+++ packages/app/components/Topbar.jsx
@@ -1,19 +1,32 @@
 import Link from "next/link";
 import React from "react";
+import { DiGithubBadge } from "react-icons/di";
 
 function Topbar() {
   return (
-    <Link href="/" className="w-full px-4 py-3 flex items-center justify-start">
-      <img
-        src="/logo.png"
-        width={0}
-        height={0}
-        className="w-[50px] "
-        alt="nocred logo"
-        placeholder="empty"
-      />
-      <h1 className="text-green-200 font-ppB ">Nocred</h1>
-    </Link>
+    <div className="w-full flex items-center justify-between relative">
+      <Link
+        href="/"
+        className="w-auto px-4 py-3 flex items-center justify-start"
+      >
+        <img
+          src="/logo.png"
+          width={0}
+          height={0}
+          className="w-[50px] "
+          alt="nocred logo"
+          placeholder="empty"
+        />
+        <h1 className="text-green-200 font-ppB ">Nocred</h1>
+      </Link>
+      <a
+        target="_blank"
+        href="https://github.com/benrobo/nocred"
+        className="font-ppB underline ml-2"
+      >
+        <DiGithubBadge className="text-white-100" size={20} />
+      </a>
+    </div>
   );
 }
 
